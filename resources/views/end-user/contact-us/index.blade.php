4ace45f3fa859b2f190de8d396dce2e946795cac
Pakpahan02 <edwardpakpahan222@gmail.com>
2025-09-06 18:13:43 +0700

    update (contact-us) : change address

--- resources/views/end-user/contact-us/index.blade.php
+++ resources/views/end-user/contact-us/index.blade.php
@@ -12,19 +12,17 @@
                 <div class="space-y-4 mb-6">
                     <h3 class="text-xl font-semibold text-gray-800">Hubungi Kami</h3>
                     <p class="text-gray-600">
-                        Jl. Letjen R. Soeprapto No. 10 B, Cempaka Putih Timur, Jakarta Pusat, DKI Jakarta,
-                        Indonesia.
+                        Jl. Brantas Raya No.26, RT.003/RW.014, Jatisari, Kec. Jatiasih, Kota Bks, Jawa Barat 17426
                     </p>
                     <p class="text-sm text-gray-500">
                         <span class="block">Lihat peta lebih besar</span>
-                        <span class="block">4.2 ⭐ (28 reviews)</span>
+                        <span class="block">5.0 ⭐ (3 reviews)</span>
                     </p>
                 </div>
 
                 {{-- Placeholder untuk Google Maps Iframe --}}
                 <div class="w-full h-[400px] rounded-lg overflow-hidden border border-gray-200">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.526279930726!2d106.87413471476906!3d-6.195079695509795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f53839211327%3A0x7d066b1d42a98f7e!2sKementerian%20Ketenagakerjaan%20Republik%20Indonesia!5e0!3m2!1sid!2sid!4v1628173428987!5m2!1sid!2sid"
-                            width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3573.1167851249998!2d106.95209558968939!3d-6.333759940023958!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69931bafe7cc45%3A0xc5fe5a2d8ff998d3!2sWorkshop%20PT.%20BWI!5e0!3m2!1sid!2sid!4v1757157122933!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </div>
